0fa71f52d0258e7015c9fe8315cd9ca3cadb8144
Anatoly Trosinenko <anatoly.trosinenko@gmail.com>
2016-08-07 17:41:38 +0300

    Generate helper wrappers with correct signature.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: libresolv.so libthread.so libother.so
+all: libresolv.so libthread.so libother.so include/wrappers.h
 
 libresolv.so: res_query.c
 	$(CC) $(CFLAGS) --shared res_query.c -fPIC -o libresolv.so
@@ -9,5 +9,9 @@ libthread.so: thread.c
 libother.so: other.c
 	$(CC) $(CFLAGS) --shared other.c -fPIC -o libother.so
 
+include/wrappers.h:
+	mkdir -p include
+	gcc -E hlp.h -I ../qemu/include/ -I ../qemu/build-emscripten/ | sed -r 's/(DEF_HELPER)/\n\1/g' | ./gen_helper_wrappers.py > include/wrappers.h
+
 clean:
-	rm -f *.so
+	rm -f *.so include/wrappers.h
---
+++ gen_helper_wrappers.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+from pyparsing import *
+
+from sys import stdin
+
+ident = Word(alphanums + '_')
+helper_name = ident('name')
+flags = CharsNotIn("),")('flags')
+delim = Suppress(ZeroOrMore(' ') + ',' + ZeroOrMore(' '))
+typeDecl = (ZeroOrMore(delim + ident))('types')
+
+def_helper = Suppress('DEF_HELPER_FLAGS_' + Word(nums) + '(') + helper_name + delim + flags + typeDecl + Suppress(')' + ZeroOrMore(' '))
+
+sizes = {
+    'int': 1,
+    'env': 1,
+    'i32': 1,
+    'i64': 2,
+    's32': 1,
+    's64': 2,
+    'tl': 1,
+    'ptr': 1,
+    'MMXReg': 1,
+    'XMMReg': 1,
+}
+
+def gen_call(name, types):
+    res = "helper_" + name + "("
+    cur = 1
+    first = True
+    for t in types:
+        if not first:
+            res += ", "
+        res += "0"
+        sz = sizes[t]
+        for i in xrange(sz):
+            res += " | (((long long)arg" + str(cur + i) + ") << " + str(i * 32) + ")"
+        cur += sz
+        first = False
+    return res + ")"
+    
+print "#ifndef WRAPPERS_H"
+print "#define WRAPPERS_H"
+for line in stdin:
+    try:
+        helper = def_helper.parseString(line)
+        print "long long " + helper.name + "_wrapper(int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7, int arg8, int arg9, int arg10) {"
+        if helper.types[0] == "void":
+            print "\t" + gen_call(helper.name, helper.types[1:]) + ";"
+            print "\treturn 0;"
+        else:
+            print "\treturn " + gen_call(helper.name, helper.types[1:]) + ";"
+        print "}"
+    except ParseException as e:
+        pass
+print "#endif"
